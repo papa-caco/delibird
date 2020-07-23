4eb8a964167196d4504c4329c9466606a87b852a
papa-caco <ccomesa@gmail.com>
2020-07-23 19:16:50 -0300

    [GameCard] Listo los faltantes - Crea estructura Tall-Grass completa y Solucionado Reintento Operación con archivos abiertos

---
+++ gameboy/bin/gc-varios.sh
@@ -0,0 +1,111 @@
+./gameboy BROKER NEW_POKEMON Blastoise 56 23 89
+./gameboy BROKER NEW_POKEMON Bulbasaur 56 23 89
+./gameboy BROKER NEW_POKEMON Charmeleon 56 23 89
+./gameboy BROKER NEW_POKEMON Charmander 56 23 89
+./gameboy BROKER NEW_POKEMON Charizard 56 23 89
+./gameboy BROKER NEW_POKEMON Ivysaur 56 23 89
+./gameboy BROKER NEW_POKEMON Pikachu 56 23 89
+./gameboy BROKER NEW_POKEMON Squirtle 56 23 89
+./gameboy BROKER NEW_POKEMON Venusaur 16 27 38
+./gameboy BROKER NEW_POKEMON Wartortle 16 27 38
+./gameboy BROKER NEW_POKEMON Blastoise 16 27 38
+./gameboy BROKER NEW_POKEMON Bulbasaur 16 27 38
+./gameboy BROKER NEW_POKEMON Charmeleon 16 27 38
+./gameboy BROKER NEW_POKEMON Charmander 16 27 38
+./gameboy BROKER NEW_POKEMON Charizard 16 27 38
+./gameboy BROKER NEW_POKEMON Ivysaur 16 27 38
+./gameboy BROKER NEW_POKEMON Pikachu 24 35 47
+./gameboy BROKER NEW_POKEMON Squirtle 24 35 47
+./gameboy BROKER NEW_POKEMON Venusaur 24 35 47
+./gameboy BROKER NEW_POKEMON Wartortle 24 35 47
+./gameboy BROKER NEW_POKEMON Blastoise 24 35 47
+./gameboy BROKER NEW_POKEMON Bulbasaur 24 35 47
+./gameboy BROKER NEW_POKEMON Charmeleon 24 35 47
+./gameboy BROKER NEW_POKEMON Charmander 24 35 47
+./gameboy BROKER NEW_POKEMON Charizard 20 39 48
+./gameboy BROKER NEW_POKEMON Ivysaur 20 39 48
+./gameboy BROKER NEW_POKEMON Pikachu 20 39 48
+./gameboy BROKER NEW_POKEMON Squirtle 20 39 48
+./gameboy BROKER NEW_POKEMON Venusaur 20 39 48
+./gameboy BROKER NEW_POKEMON Wartortle 20 39 48
+./gameboy BROKER NEW_POKEMON Blastoise 20 39 48
+./gameboy BROKER NEW_POKEMON Bulbasaur 20 39 48
+./gameboy BROKER NEW_POKEMON Charmeleon 21 45 67
+./gameboy BROKER NEW_POKEMON Charmander 21 45 67
+./gameboy BROKER NEW_POKEMON Charizard 21 45 67
+./gameboy BROKER NEW_POKEMON Ivysaur 21 45 67
+./gameboy BROKER NEW_POKEMON Pikachu 21 45 67
+./gameboy BROKER NEW_POKEMON Squirtle 21 45 67
+./gameboy BROKER NEW_POKEMON Venusaur 21 45 67
+./gameboy BROKER NEW_POKEMON Wartortle 21 45 67
+./gameboy BROKER NEW_POKEMON Blastoise 32 54 76
+./gameboy BROKER NEW_POKEMON Bulbasaur 32 54 76
+./gameboy BROKER NEW_POKEMON Charmeleon 32 54 76
+./gameboy BROKER NEW_POKEMON Charmander 32 54 76
+./gameboy BROKER NEW_POKEMON Charizard 32 54 76
+./gameboy BROKER NEW_POKEMON Ivysaur 32 54 76
+./gameboy BROKER NEW_POKEMON Pikachu 32 54 76
+./gameboy BROKER NEW_POKEMON Squirtle 32 54 76
+./gameboy BROKER NEW_POKEMON Venusaur 87 61 73
+./gameboy BROKER NEW_POKEMON Wartortle 87 61 73
+./gameboy BROKER NEW_POKEMON Blastoise 87 61 73
+./gameboy BROKER NEW_POKEMON Bulbasaur 87 61 73
+./gameboy BROKER NEW_POKEMON Charmeleon 87 61 73
+./gameboy BROKER NEW_POKEMON Charmander 87 61 73
+./gameboy BROKER NEW_POKEMON Charizard 87 61 73
+./gameboy BROKER NEW_POKEMON Ivysaur 87 61 73
+./gameboy BROKER NEW_POKEMON Pikachu 97 64 31
+./gameboy BROKER NEW_POKEMON Squirtle 97 64 31
+./gameboy BROKER NEW_POKEMON Venusaur 97 64 31
+./gameboy BROKER NEW_POKEMON Wartortle 97 64 31
+./gameboy BROKER NEW_POKEMON Blastoise 97 64 31
+./gameboy BROKER NEW_POKEMON Bulbasaur 97 64 31
+./gameboy BROKER NEW_POKEMON Charmeleon 97 64 31
+./gameboy BROKER NEW_POKEMON Charmander 97 64 31
+./gameboy BROKER NEW_POKEMON Charizard 59 72 83
+./gameboy BROKER NEW_POKEMON Ivysaur 59 72 83
+./gameboy BROKER NEW_POKEMON Pikachu 59 72 83
+./gameboy BROKER NEW_POKEMON Squirtle 59 72 83
+./gameboy BROKER NEW_POKEMON Venusaur 59 72 83
+./gameboy BROKER NEW_POKEMON Wartortle 59 72 83
+./gameboy BROKER NEW_POKEMON Blastoise 59 72 83
+./gameboy BROKER NEW_POKEMON Bulbasaur 59 72 83
+./gameboy BROKER NEW_POKEMON Charmeleon 91 74 100
+./gameboy BROKER NEW_POKEMON Charmander 91 74 100
+./gameboy BROKER NEW_POKEMON Charizard 91 74 100
+./gameboy BROKER NEW_POKEMON Ivysaur 91 74 100
+./gameboy BROKER NEW_POKEMON Pikachu 91 74 100
+./gameboy BROKER NEW_POKEMON Squirtle 91 74 100
+./gameboy BROKER NEW_POKEMON Venusaur 91 74 100
+./gameboy BROKER NEW_POKEMON Wartortle 91 74 100
+./gameboy BROKER CATCH_POKEMON Blastoise 16 27
+./gameboy BROKER CATCH_POKEMON Bulbasaur 24 35
+./gameboy BROKER CATCH_POKEMON Charmeleon 20 39
+./gameboy BROKER CATCH_POKEMON Charmander 32 54
+./gameboy BROKER CATCH_POKEMON Charizard 97 64
+./gameboy BROKER CATCH_POKEMON Ivysaur 56 23
+./gameboy BROKER CATCH_POKEMON Pikachu 21 45
+./gameboy BROKER CATCH_POKEMON Squirtle 59 72
+./gameboy BROKER CATCH_POKEMON Venusaur 87 61
+./gameboy BROKER CATCH_POKEMON Wartortle 91 74
+./gameboy BROKER GET_POKEMON Blastoise #
+
+./gameboy BROKER GET_POKEMON Bulbasaur #
+
+./gameboy BROKER GET_POKEMON Charmeleon #
+
+./gameboy BROKER GET_POKEMON Charmander #
+
+./gameboy BROKER GET_POKEMON Charizard #
+
+./gameboy BROKER GET_POKEMON Ivysaur #
+
+./gameboy BROKER GET_POKEMON Pikachu #
+
+./gameboy BROKER GET_POKEMON Squirtle #
+
+./gameboy BROKER GET_POKEMON Venusaur #
+
+./gameboy BROKER GET_POKEMON Wartortle #
+
+
