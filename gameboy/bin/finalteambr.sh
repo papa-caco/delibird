737cfe861fc9bb97e564c6cd866936b04932f427
papa-caco <ccomesa@gmail.com>
2020-07-25 16:12:10 -0300

    TEAM Final

---
+++ gameboy/bin/finalteambr.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+./gameboy BROKER APPEARED_POKEMON Pikachu 9 9 1
+./gameboy BROKER APPEARED_POKEMON Squirtle 6 7 2
+./gameboy BROKER APPEARED_POKEMON Onix 2 8 3
+./gameboy BROKER APPEARED_POKEMON Gengar 6 6 4 
+./gameboy BROKER APPEARED_POKEMON Squirtle 5 5 5
